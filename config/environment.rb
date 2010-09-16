d3a3f365a71a3101bd7a96556748c0ee469f32c5
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2010-09-17 01:50:13 +0200

    Added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN'],
+}
